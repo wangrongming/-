fb211aea96443dcf736102b1a93aaaca7372aa6b
wangrongming <1768389896@qq.com>
2019-10-17 11:55:28 +0800

    时间模块基础用法

---
+++ time&datetime/time_deal.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import datetime
+
+# 使用datetime模块来获取当前的日期和时间
+# 参数如下：
+cur = datetime.datetime.now()
+print(cur, cur.isoformat())
+print(cur.year)
+print(cur.month)
+print(cur.day)
+print(cur.hour)
+print(cur.minute)
+print(cur.second)
+
+
+# 当前时间前一天
+d = datetime.datetime.now()
+one_day = datetime.timedelta(days=1)
+yesterday = d - one_day
+print(yesterday)
+
+# 当前时间标准化
+info = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print(info)
+
+# 字符转时间
+str_time = datetime.datetime.strptime("2019-10-11 11:26:34", '%Y-%m-%d %H:%M:%S')
+print(str_time)
+print(int(str_time.timestamp()))
+
