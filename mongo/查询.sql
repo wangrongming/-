e35f8b994c4720ea94ec42f1684be1f12d119706
wangrongming <1768389896@qq.com>
2019-10-21 17:32:47 +0800

    mongo

---
+++ mongo/查询.sql
@@ -0,0 +1,35 @@
+-- 1 mongo嵌套查询
+
+-- 1 mongo嵌套查询
+db.storage.find({
+    'items.category': {
+        $eq: 'food'
+    }
+})
+
+-- 2 返回一个结果
+db.getCollection('jdkf').find(
+    {
+    'element.chatLogMessageList.content': {
+            $eq: '许愿今天发货🙏'
+        }
+    },
+    {
+        'items.$': 1
+    }
+)
+
+-- 3 返回多查询结果
+db.storage.aggregate( {
+    $project: {
+        "items": {
+            $filter: {
+                input: "$items",
+                as: "item",
+                cond: {
+                    $eq: [ '$$item.category', 'food' ]
+                }
+            }
+        }
+    }
+} )
