015c7a588811aeef6949a03e91f602de2fe5d287
wangrongming <1768389896@qq.com>
2020-01-03 17:17:14 +0800

    syspath

---
+++ python模块基础用法/syspath.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ @Time : 2020/1/3 17:16
+ @Auth : 明明
+ @IDE  : PyCharm
+ """
+import sys
+
+sys.path.append(sys.path[0])
+sys.path.append(sys.path[0] + '/..')
+sys.path.append(sys.path[0] + '/../..')
