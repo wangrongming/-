22a18d58fd6caad9e90273cfba4cd193ad813918
wangrongming <1768389896@qq.com>
2020-01-20 17:27:30 +0800

    syspath

--- python模块基础用法/syspath.py
+++ python模块基础用法/syspath.py
@@ -10,3 +10,7 @@ import sys
 sys.path.append(sys.path[0])
 sys.path.append(sys.path[0] + '/..')
 sys.path.append(sys.path[0] + '/../..')
+
+# print(range(2, int(1 + 1)))
+for i in range(2, 2):
+    print(i)
