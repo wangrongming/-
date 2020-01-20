34a64772ddf38304bff0cb6412c6705f870c0a2a
wangrongming <1768389896@qq.com>
2020-01-20 17:04:32 +0800

    异步同步：多进程用法

--- python模块基础用法/thread_block.py
+++ python模块基础用法/thread_block.py
@@ -1,12 +1,56 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ @Time : 2020/1/20 16:26
+ @Auth : 明明
+ @IDE  : PyCharm
+ """
+# 多线程
 import threading
+from multiprocessing import dummy
 
 
-def thread_test(i):
-	print("当前是线程{}".format(i))
+def task(i):
+    print("当前传入参数为{}".format(i))
+
+
+def thread_one():
+    li = []
+
+    for i in range(5):
+        t = threading.Thread(target=task, args=(i,))
+        li.append(t)
+        t.start()
+        t.join()
+
+    for t in li:
+        t.join()
+
+
+def thread_two():
+    """
+    1 非阻塞方法
+        multiprocessing.dummy.Pool.apply_async() 和 multiprocessing.dummy.Pool.imap()
+        线程并发执行
+
+    2 阻塞方法
+        multiprocessing.dummy.Pool.apply()和 multiprocessing.dummy.Pool.map()
+        线程顺序执行
+    """
+    pool = dummy.Pool(4)
+    chat_arg_list = [1, 2, 3, 4, 5]
+
+    for i in range(1, 6):
+        pool.apply_async(task, args=(i,))  # 非阻塞 有返回值
+        pool.apply(task, args=(i,))  # 阻塞 无返回值
+
+    # 非阻塞
+    pool.imap(task, chat_arg_list)  # 非阻塞 无返回值
+    pool.map(task, chat_arg_list)  # 阻塞 无返回值
+
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
-	for i in range(5):
-		t = threading.Thread(target=thread_test, args=(i,))
-		t.start()
-	t.join()
+    thread_two()
